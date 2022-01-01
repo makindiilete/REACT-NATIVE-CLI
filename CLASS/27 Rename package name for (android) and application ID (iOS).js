3cf2bd4f5421c3f2192b4cbef07daeafd1deb680
michaelzomoakin <44997832+makindiilete@users.noreply.github.com>
2022-01-01 17:02:49 +0100

    change package name (android) and application ID (ios)

---
+++ CLASS/27 Rename package name for (android) and application ID (iOS).js
@@ -0,0 +1,7 @@
+/*
+package name (android) and application ID (ios) are the two values that uniquely identifies our app on app store and playstore and by default react native generates one for us but we might need to change it..
+
+For this app, the default generated on is 'com.rncontacts' but we want to change it to 'com.makindiilete.rncontacts'
+
+We can easily do this using find and replace feature and then rebuild our app
+*/
--- rncontacts/android/app/src/main/java/com/rncontacts/MainActivity.java
+++ rncontacts/android/app/src/main/java/com/rncontacts/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rncontacts;
+package com.makindiilete.rncontacts;
 
 import com.facebook.react.ReactActivity;
 
