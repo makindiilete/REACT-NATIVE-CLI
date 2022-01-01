5a80e1f7f525f35e38cd6dadae32710de10377be
michaelzomoakin <44997832+makindiilete@users.noreply.github.com>
2022-01-01 17:09:21 +0100

    28 Change app display name

---
+++ CLASS/28 Change App Display name.js
@@ -0,0 +1,9 @@
+/*
+We want to change our app name from 'rncontacts' to 'RN Contacts'
+
+Android
+Find the file (use search everywhere) 'strings.xml'
+
+IOS
+Find the file 'info.plists' >> Look for 'CFBundleDisplayName' and change the name under it
+*/
