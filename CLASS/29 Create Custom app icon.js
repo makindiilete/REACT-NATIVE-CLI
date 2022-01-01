6c5aad9e63257e6b4680fd8146bf0ed8af9184b3
michaelzomoakin <44997832+makindiilete@users.noreply.github.com>
2022-01-01 18:06:12 +0100

    Creating the settinfs UI screen

--- CLASS/29 Create Custom app icon.js
+++ CLASS/29 Create Custom app icon.js
@@ -1,4 +1,11 @@
 /*
 ANDROID
 1-  Open the android folder in android studio >> Go to '`app name` >> app >> src >> main >> res >> right click on the 'res' folder >> New >> Image assets >> Change d default android image by click on the folder icon on the 'path' input field on the 'Foreground layer' tab >> Click on the 'Background Layer' tab >> Change assets type to 'Color' and change the color to white background or any color of your choice >. Click Next >> Click Finish
+
+IOS
+1-  Visit https://appicon.co/
+2-  Select the image you want to use and check 'iphone' and 'iPad'
+3-  Click on 'Generate Assets'
+4-  Open your project in Xcode >> Click on the project name https://prnt.sc/25lb9n3
+5-  Drag the icons onto the boxes https://prnt.sc/25lbyvo
 */
--- rncontacts/src/screens/Settings.js
+++ rncontacts/src/screens/Settings.js
@@ -1,10 +1,70 @@
-import {Text, View} from 'react-native';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
+import AppMsgComponent from '../components/AppMsgComponent';
+import {AppContainer} from '../components/AppContainer';
+import {colors} from '../assets/themes/colors';
+import AppIcon from '../components/AppIcon';
 
+const settingsOptions = [
+  {title: 'My Info', subtitle: 'Setup your profile'},
+  {title: 'Accounts', subtitle: null},
+  {
+    title: 'Default account for new contacts',
+    subtitle: 'akindiileteforex@gmail.com',
+  },
+  {title: 'Contacts to display', subtitle: 'All contacts'},
+  {title: 'Sort by', subtitle: 'Last Name'},
+  {title: 'Name format', subtitle: 'Last Name first'},
+  {title: 'Import', subtitle: null},
+  {title: 'Export', subtitle: null},
+  {title: 'Blocked numbers', subtitle: null},
+  {title: 'About Contacts', subtitle: null},
+];
 export const Settings = () => {
+  const renderItem = ({item}) => {
+    const {title, subtitle} = item;
+    return (
+      <TouchableOpacity style={styles.itemContainer}>
+        <Text style={styles.title}>{title}</Text>
+        {subtitle && <Text style={styles.subtitle}>{subtitle}</Text>}
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View>
-      <Text>Hi, from Settings</Text>
-    </View>
+    <AppContainer>
+      <FlatList
+        data={settingsOptions}
+        renderItem={renderItem}
+        keyExtractor={(item) => String(item.title)}
+        showsVerticalScrollIndicator={false}
+        ListFooterComponent={<View style={{height: 50}} />}
+        style={{minHeight: '100%'}}
+      />
+    </AppContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    justifyContent: 'center',
+    marginVertical: 10,
+    borderBottomColor: '#C4C4C4',
+    borderBottomWidth: 0.3,
+    paddingBottom: 10,
+  },
+  title: {
+    fontSize: 17,
+    marginBottom: 10,
+  },
+  subtitle: {
+    opacity: 0.6,
+  },
+});
