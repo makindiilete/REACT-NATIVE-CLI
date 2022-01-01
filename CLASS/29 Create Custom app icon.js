975e61e58546ba3c078401acaf4e36844e593e32
michaelzomoakin <44997832+makindiilete@users.noreply.github.com>
2022-01-01 17:44:40 +0100

    Creating app icons

---
+++ CLASS/29 Create Custom app icon.js
@@ -0,0 +1,4 @@
+/*
+ANDROID
+1-  Open the android folder in android studio >> Go to '`app name` >> app >> src >> main >> res >> right click on the 'res' folder >> New >> Image assets >> Change d default android image by click on the folder icon on the 'path' input field on the 'Foreground layer' tab >> Click on the 'Background Layer' tab >> Change assets type to 'Color' and change the color to white background or any color of your choice >. Click Next >> Click Finish
+*/
